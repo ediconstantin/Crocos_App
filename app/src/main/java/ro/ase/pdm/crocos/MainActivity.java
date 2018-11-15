a9ca45c50b28bb858364c4caa937ff3f4c244567
alexandru.demidov <alexanderdem11@gmail.com>
2018-11-15 15:39:50 +0200

    Added student main activity

--- app/src/main/java/ro/ase/pdm/crocos/MainActivity.java
+++ app/src/main/java/ro/ase/pdm/crocos/MainActivity.java
@@ -1,13 +1,16 @@
 package ro.ase.pdm.crocos;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.client.Firebase;
@@ -15,14 +18,15 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText nEmailField;
     private EditText nPasswordField;
     private Button nLoginBtn;
+    private ProgressBar nLoginProgress;
     private FirebaseAuth nAuth;
-    private FirebaseAuth.AuthStateListener nAutchListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,32 +35,41 @@ public class MainActivity extends AppCompatActivity {
 
         nAuth = FirebaseAuth.getInstance();
 
-        nEmailField = findViewById(R.id.etEmail);
-        nPasswordField = findViewById(R.id.etPass);
-        nLoginBtn = findViewById(R.id.btnLogin);
-
-        nAutchListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if(firebaseAuth.getCurrentUser()!= null){
-                    if(nEmailField.getText().toString().toLowerCase().contains("@csie.ase.ro")){
-                        startActivity(new Intent(MainActivity.this, TeacherActivity.class));
-                    }
-                    if(nEmailField.getText().toString().toLowerCase().contains("@stud.ase.ro")){
-                        startActivity(new Intent(MainActivity.this,StudentActivity.class));
-                    }
-                    nEmailField.setText("");
-                    nPasswordField.setText("");
-                }
-            }
-        };
+        nEmailField = (EditText)findViewById(R.id.etEmail);
+        nPasswordField = (EditText)findViewById(R.id.etPass);
+        nLoginBtn = (Button)findViewById(R.id.btnLogin);
+        nLoginProgress = (ProgressBar)findViewById(R.id.login_progress);
 
 
         nLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startSignIn();
-            }
+                String email = nEmailField.getText().toString();
+                String pass = nPasswordField.getText().toString();
+
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(pass)){
+                    Toast.makeText(MainActivity.this, "At least one field is empty", Toast.LENGTH_LONG).show();
+                }
+
+                else if(!TextUtils.isEmpty(email) || !TextUtils.isEmpty(pass)){
+                    nLoginProgress.setVisibility(View.VISIBLE);
+                    nAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()) {
+                                sentToMainScene();
+                            }
+                            else {
+                                String errormessage = task.getException().getMessage();
+                                Toast.makeText(MainActivity.this, errormessage, Toast.LENGTH_LONG).show();
+                            }
+                            nLoginProgress.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                }
+
+                }
         });
 
     }
@@ -64,25 +77,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        nAuth.addAuthStateListener(nAutchListener);
+        FirebaseUser currentUser = nAuth.getCurrentUser();
+        if(currentUser!=null){
+            sentToMainScene();
+        }
     }
 
-    private void startSignIn(){
-        String email = nEmailField.getText().toString();
-        String pass = nPasswordField.getText().toString();
+    private void sentToMainScene(){
+        if(nEmailField.getText().toString().toLowerCase().contains("@csie.ase.ro")){
+            startActivity(new Intent(MainActivity.this, TeacherActivity.class));
+            finish();
+        }
+        if(nEmailField.getText().toString().toLowerCase().contains("@stud.ase.ro")){
+            startActivity(new Intent(MainActivity.this,StudentActivity.class));
+            finish();
+        }
+    }
 
-        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(pass)){
-            Toast.makeText(MainActivity.this, "At least one field is empty", Toast.LENGTH_LONG).show();
-        }
-        else {
-            nAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (!task.isSuccessful()) {
-                        Toast.makeText(MainActivity.this, "Sign-in problem", Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
-        }
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
     }
 }
--- app/src/main/java/ro/ase/pdm/crocos/StudentActivity.java
+++ app/src/main/java/ro/ase/pdm/crocos/StudentActivity.java
@@ -1,8 +1,13 @@
 package ro.ase.pdm.crocos;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,23 +15,55 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class StudentActivity extends AppCompatActivity {
 
-    private Button nSignOut;
+    private BottomNavigationView mainBottomNav;
+
+    private JoinFragment joinFragment;
+    private HistoryFragment historyFragment;
+    private ProfileFragment profileFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
-        nSignOut = findViewById(R.id.btnSignOut);
 
-        nSignOut.setOnClickListener(new View.OnClickListener() {
+        mainBottomNav = (BottomNavigationView)findViewById(R.id.bottomNavBar);
+        mainBottomNav.setSelectedItemId(R.id.itmJoin);
+
+
+        joinFragment = new JoinFragment();
+        historyFragment = new HistoryFragment();
+        profileFragment = new ProfileFragment();
+
+        replaceFragment(joinFragment);
+
+        mainBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                finish();
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.itmJoin:
+                        replaceFragment(joinFragment);
+                        return true;
+                    case R.id.itmProfile:
+                        replaceFragment(profileFragment);
+                        return true;
+                    case R.id.itmHistory:
+                        replaceFragment(historyFragment);
+                        return true;
+                        default:
+                            return false;
+                }
             }
         });
 
+
     }
 
+    private void replaceFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.mainContainer, fragment);
+        fragmentTransaction.commit();
+
+    }
 
 
 
