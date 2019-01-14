6d7e5b22023975fbfe32ce08948f1be47980c668
alexandru.demidov <alexanderdem11@gmail.com>
2019-01-14 14:29:47 +0200

    Validation for pop-up

--- app/src/main/java/ro/ase/pdm/crocos/QuestionsActivity.java
+++ app/src/main/java/ro/ase/pdm/crocos/QuestionsActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -39,6 +42,8 @@ public class QuestionsActivity extends AppCompatActivity implements Constant {
     private Test test;
     private ListViewAdapter listViewAdapter;
     private List<Question> allQuestions;
+    EditText etQuestion, etAns1, etAns2, etAns3,etAns4;
+    Button btnAddQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +55,7 @@ public class QuestionsActivity extends AppCompatActivity implements Constant {
         btn = findViewById(R.id.floatingActionButton);
         Button btnSave = findViewById(R.id.btnSave);
 
+        validation();
         initData();
 
         listView = findViewById(R.id.lv);
@@ -97,6 +103,22 @@ public class QuestionsActivity extends AppCompatActivity implements Constant {
 
         //questions will be sent to be affiliated with the test one by one
         //the finish button will only close the activity and will redirect to tests_activity
+    }
+
+    private void validation(){
+        etQuestion = findViewById(R.id.etQuestion);
+        etAns1 = findViewById(R.id.etAns1);
+        etAns2 = findViewById(R.id.etAns2);
+        etAns3 = findViewById(R.id.etAns3);
+        etAns4 = findViewById(R.id.etAns4);
+        btnAddQuestion = findViewById(R.id.btnAddQuestion);
+
+        etQuestion.addTextChangedListener(textWatcher);
+        etAns1.addTextChangedListener(textWatcher);
+        etAns2.addTextChangedListener(textWatcher);
+        etAns3.addTextChangedListener(textWatcher);
+        etAns4.addTextChangedListener(textWatcher);
+
     }
 
     private void showPopUp(){
@@ -147,5 +169,29 @@ public class QuestionsActivity extends AppCompatActivity implements Constant {
     private void disableCircle(){
         //this function will hide the rotating circle
     }
+
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String questionInput = etQuestion.getText().toString();
+            String ans1Input = etAns1.getText().toString();
+            String ans2Input = etAns2.getText().toString();
+            String ans3Input = etAns3.getText().toString();
+            String ans4Input = etAns4.getText().toString();
+
+            btnAddQuestion.setEnabled(!questionInput.isEmpty() && !ans1Input.isEmpty()
+                    && !ans2Input.isEmpty() && !ans3Input.isEmpty() && !ans4Input.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 
 }
