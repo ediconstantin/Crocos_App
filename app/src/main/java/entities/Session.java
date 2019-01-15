36c583b1f5920333692e02402191df4618b34ec2
alexandru.demidov <alexanderdem11@gmail.com>
2019-01-15 19:19:38 +0200

    Layout for sessions

---
+++ app/src/main/java/entities/Session.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class Session {
+
+    private int status;
+    private String name;
+
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
--- app/src/main/java/ro/ase/pdm/crocos/SessionActivity.java
+++ app/src/main/java/ro/ase/pdm/crocos/SessionActivity.java
@@ -5,23 +5,58 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Session;
 
 public class SessionActivity extends AppCompatActivity {
 
     FloatingActionButton fabNewSession;
+    ListView lvSession;
+    SessionAdapter adapter;
+    List<Session> sessions= new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session);
 
+        lvSession = findViewById(R.id.lvSession);
+        initData();
+
         fabNewSession = findViewById(R.id.fabNewSession);
         fabNewSession.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SessionActivity.this,CreateSessionActivity.class));
+                startActivity(new Intent(SessionActivity.this,TestActivity.class));
             }
         });
 
+
+        adapter = new SessionAdapter(this, R.layout.session_item, sessions);
+        lvSession.setAdapter(adapter);
+    }
+
+    private void initData(){
+        Session s1 = new Session();
+        s1.setName("First session");
+        s1.setStatus(1);
+
+        Session s2 = new Session();
+        s2.setName("Second session");
+        s2.setStatus(2);
+
+        Session s3 = new Session();
+        s3.setName("Third session");
+        s3.setStatus(0);
+
+        sessions.add(s1);
+        sessions.add(s2);
+        sessions.add(s3);
 
     }
 }
