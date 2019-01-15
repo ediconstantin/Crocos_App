442d840b9cc600f7fa5c4c4d8b9a06251a29a3ae
alexandru.demidov <alexanderdem11@gmail.com>
2019-01-15 20:32:28 +0200

    Added button in navbar for sessions

--- app/src/main/java/ro/ase/pdm/crocos/TeacherActivity.java
+++ app/src/main/java/ro/ase/pdm/crocos/TeacherActivity.java
@@ -11,6 +11,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import entities.Session;
+
 
 public class TeacherActivity extends AppCompatActivity {
 
@@ -37,6 +39,9 @@ public class TeacherActivity extends AppCompatActivity {
                         return true;
                     case R.id.itmTest:
                         startActivity(new Intent(TeacherActivity.this, TestActivity.class));
+                        return true;
+                    case R.id.itmSessions:
+                        startActivity(new Intent(TeacherActivity.this,Session.class));
                         return true;
                     default:
                         return false;
